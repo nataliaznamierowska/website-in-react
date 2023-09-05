c80b69e933750427592b63afdce1bffd32a01c63
natalia_znamierowska <nat.znamierowska@gmail..com>
2023-09-05 20:52:27 +0200

    xd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import AboutCss from "./styles/About.css";
-import FooterCss from "./styles/Footer.css";
-import HeaderCss from "./styles/Header.css";
-import NavbarCss from "./styles/Navbar.css";
-import OfferCss from"./styles/Offer.css";
-import AppCss from "./styles/App.css";
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
